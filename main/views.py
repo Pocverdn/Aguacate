4f67daddf18630cc3f3d6358d109bb2fd0f54508
javasquezt <javasquezt@eafit.edu.co>
2025-02-07 15:48:17 -0500

    botones de los metodos

--- main/views.py
+++ main/views.py
@@ -20,3 +20,69 @@ def metodo1(request):
 
 def metodo2(request):
     return render(request, "metodo2.html")
+
+def metodo3(request):
+    return render(request, "metodo3.html")
+
+def metodo4(request):
+    return render(request, "metodo4.html")
+
+def metodo5(request):
+    return render(request, "metodo5.html")
+
+def metodo6(request):
+    return render(request, "metodo6.html")
+
+def metodo7(request):
+    return render(request, "metodo7.html")
+
+def metodo8(request):
+    return render(request, "metodo8.html")
+
+def metodo9(request):
+    return render(request, "metodo9.html")
+
+def metodo10(request):
+    return render(request, "metodo10.html")
+
+def metodo11(request):
+    return render(request, "metodo11.html")
+
+def metodo12(request):
+    return render(request, "metodo12.html")
+
+def metodo13(request):
+    return render(request, "metodo13.html")
+
+def metodo14(request):
+    return render(request, "metodo14.html")
+
+def metodo15(request):
+    return render(request, "metodo15.html")
+
+def metodo16(request):
+    return render(request, "metodo16.html")
+
+def metodo17(request):
+    return render(request, "metodo17.html")
+
+def metodo18(request):
+    return render(request, "metodo18.html")
+
+def metodo19(request):
+    return render(request, "metodo19.html")
+
+def metodo20(request):
+    return render(request, "metodo20.html")
+
+def metodo21(request):
+    return render(request, "metodo21.html")
+
+def metodo22(request):
+    return render(request, "metodo22.html")
+
+def metodo23(request):
+    return render(request, "metodo23.html")
+
+def metodo24(request):
+    return render(request, "metodo24.html")
