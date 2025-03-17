84e62aa2808e2c3f6b98bbf2e763c89b4fcc52eb
jjvasquezg <juanjos137k@gmail.com>
2025-03-17 14:23:04 -0500

    Actualizados los métodos para el Cap 1

--- main/views.py
+++ main/views.py
@@ -13,79 +13,25 @@ def metodos(request):
         return render(request, "metodos.html")
     if request.method == "POST":
         print("hello0")
-        return redirect("/metodo1/")
+        return redirect("/biseccion/")
 
-def acercade(request):
-    return render(request, "acercade.html")
+def acerca_de(request):
+    return render(request, "acerca_de.html")
     
-def metodo1(request):
-    return render(request, "metodo1.html")
+def biseccion(request):
+    return render(request, "biseccion.html")
 
-def metodo2(request):
-    return render(request, "metodo2.html")
+def regla_falsa(request):
+    return render(request, "regla_falsa.html")
 
-def metodo3(request):
-    return render(request, "metodo3.html")
+def punto_fijo(request):
+    return render(request, "punto_fijo.html")
 
-def metodo4(request):
-    return render(request, "metodo4.html")
+def newton(request):
+    return render(request, "newton.html")
 
-def metodo5(request):
-    return render(request, "metodo5.html")
+def secante(request):
+    return render(request, "secante.html")
 
-def metodo6(request):
-    return render(request, "metodo6.html")
-
-def metodo7(request):
-    return render(request, "metodo7.html")
-
-def metodo8(request):
-    return render(request, "metodo8.html")
-
-def metodo9(request):
-    return render(request, "metodo9.html")
-
-def metodo10(request):
-    return render(request, "metodo10.html")
-
-def metodo11(request):
-    return render(request, "metodo11.html")
-
-def metodo12(request):
-    return render(request, "metodo12.html")
-
-def metodo13(request):
-    return render(request, "metodo13.html")
-
-def metodo14(request):
-    return render(request, "metodo14.html")
-
-def metodo15(request):
-    return render(request, "metodo15.html")
-
-def metodo16(request):
-    return render(request, "metodo16.html")
-
-def metodo17(request):
-    return render(request, "metodo17.html")
-
-def metodo18(request):
-    return render(request, "metodo18.html")
-
-def metodo19(request):
-    return render(request, "metodo19.html")
-
-def metodo20(request):
-    return render(request, "metodo20.html")
-
-def metodo21(request):
-    return render(request, "metodo21.html")
-
-def metodo22(request):
-    return render(request, "metodo22.html")
-
-def metodo23(request):
-    return render(request, "metodo23.html")
-
-def metodo24(request):
-    return render(request, "metodo24.html")
+def raices_multiples(request):
+    return render(request, "raices_multiples.html")
